b3c11f30811a8d2f777121c83a17ed687cbea165
Emad <talkwithemad@gmail.com>
2025-10-18 15:08:55 +0530

    Next and React app initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-left justify-items-left min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1>My Portfolio</h1>
+    </div>
+  );
+}
